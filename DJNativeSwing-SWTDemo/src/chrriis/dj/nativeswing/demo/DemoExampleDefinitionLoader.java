a120e9690963119b38c7c7a5a3ff078251d860ca
Chrriis <chrriis@gmail.com>
2008-05-01 18:50:42 +0000

    Improvements to the demo.

--- DJNativeSwing-SWTDemo/src/chrriis/dj/nativeswing/demo/DemoExampleDefinitionLoader.java
+++ DJNativeSwing-SWTDemo/src/chrriis/dj/nativeswing/demo/DemoExampleDefinitionLoader.java
@@ -46,22 +46,22 @@ public class DemoExampleDefinitionLoader {
     }));
     exampleGroupList.add(new ExampleGroup("JWebBrowser", new Example[] {
         new Example("Simple Example", SimpleWebBrowserExample.class, "This is a simple example that shows the basic configuration of an embedded web browser component.", true),
-        new Example("Setting Content", SettingContent.class, "Any HTML content can be set to the web browser.", true),
-        new Example("Javascript Execution", JavascriptExecution.class, "Javscript can be executed in the web browser component.", true),
-        new Example("Navigation Control", NavigationControl.class, "The Java application can control the navigation happening in the web browser.\nThis feature allows to block certain links and/or the creation of new windows, or to open links and/or new windows elsewhere.", true),
-        new Example("Sending Commands", SendingCommands.class, "Use static links or simple Javascript to send commands with arguments to the application:\n    function sendCommand(command) {\n      var s = 'command://' + encodeURIComponent(command);\n      for(var i=1; i<arguments.length; s+='&'+encodeURIComponent(arguments[i++]));\n      window.location = s;\n    }", true),
+        new Example("Setting Content", SettingContent.class, "Set any HTML content to the web browser.", true),
+        new Example("Javascript Execution", JavascriptExecution.class, "Execute some Javascript code in the current web browser page.", true),
+        new Example("Navigation Control", NavigationControl.class, "Control the navigation happening in the web browser from the Java application.\nThis allows to block certain links and/or the creation of new windows, or to open links and/or new windows elsewhere.", true),
+        new Example("Sending Commands", SendingCommands.class, "Use static links or simple Javascript to send some commands with arguments to the application:\n    function sendCommand(command) {\n      var s = 'command://' + encodeURIComponent(command);\n      for(var i=1; i<arguments.length; s+='&'+encodeURIComponent(arguments[i++]));\n      window.location = s;\n    }", true),
         new Example("Classpath pages", ClasspathPages.class, "Load web pages from the classpath with the help of a simple embedded web server.", true),
     }));
     exampleGroupList.add(new ExampleGroup("JFlashPlayer", new Example[] {
         new Example("Simple Example", SimpleFlashExample.class, "Display a flash application.", true),
-        new Example("Variables and Flow", VariablesAndFlow.class, "Control a flash animation, and get/set variables.", true),
-        new Example("Function Calls", FunctionCalls.class, "Invoke functions with or without results and received commands.", true),
+        new Example("Variables and Flow", VariablesAndFlow.class, "Control the flow of a flash animation, and get/set global variables.", true),
+        new Example("Function Calls", FunctionCalls.class, "Invoke functions, with or without waiting for a result, and listen to Flash commands.", true),
     }));
     exampleGroupList.add(new ExampleGroup("JVLCPlayer", new Example[] {
         new Example("Simple Example", SimpleVLCPlayerExample.class, "Load a movie/sound file to an embedded VLC player.", true),
     }));
     exampleGroupList.add(new ExampleGroup("JHTMLEditor", new Example[] {
-        new Example("Simple Example", SimpleHTMLEditorExample.class, "Edit HTML, get and set the content of the editor.", true),
+        new Example("Simple Example", SimpleHTMLEditorExample.class, "Graphically edit some HTML, get and set the HTML content.", true),
     }));
     exampleGroupList.add(new ExampleGroup("JMultiMediaPlayer (win32)", new Example[] {
         new Example("Simple Example", SimpleMultiMediaPlayerExample.class, "Load a movie/sound file to an embedded multimedia player.", true, System.getProperty("os.name").startsWith("Windows"), "This example is only available on a Windows operating system."),
@@ -70,7 +70,7 @@ public class DemoExampleDefinitionLoader {
         new Example("Constrain Visibility", ConstrainVisibility.class, "Constrain the visibility to superimpose Swing and native components.", true),
         new Example("Deferred Destruction", DeferredDestruction.class, "Defer destruction until finalization to add/remove the same component.\nIt is not destroyed when removed but on disposeNativePeer() or garbage collection.", true),
         new Example("Hierarchy Proxying", HierarchyProxying.class, "Use a proxied component hierarchy for the native components to allow re-parenting and change of component Z-order.", true),
-        new Example("Thumbnail Creation", ThumbnailCreation.class, "Create a thumbnail by painting the native component to an image.", true),
+        new Example("Thumbnail Creation", ThumbnailCreation.class, "Create a thumbnail by painting a native component to an image.", true),
         new Example("Pseudo Transparency", AlphaBlendingSimulation.class, "Simulate alpha blending of a Swing component over a native component. This works rather well over static content.", true),
     }));
     exampleGroupList.add(new ExampleGroup("Utilities", new Example[] {
