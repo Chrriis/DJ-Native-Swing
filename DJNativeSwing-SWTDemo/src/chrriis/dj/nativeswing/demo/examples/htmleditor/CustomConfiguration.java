5a57a0098934983f5408fbdb9beb85a926a4f33a
Chrriis <chrriis@gmail.com>
2008-05-28 19:37:52 +0000

    Missing main method is added.

--- DJNativeSwing-SWTDemo/src/chrriis/dj/nativeswing/demo/examples/htmleditor/CustomConfiguration.java
+++ DJNativeSwing-SWTDemo/src/chrriis/dj/nativeswing/demo/examples/htmleditor/CustomConfiguration.java
@@ -9,8 +9,12 @@ package chrriis.dj.nativeswing.demo.examples.htmleditor;
 
 import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
+import chrriis.common.UIUtils;
+import chrriis.dj.nativeswing.NativeInterface;
 import chrriis.dj.nativeswing.components.JHTMLEditor;
 
 /**
@@ -28,6 +32,22 @@ public class CustomConfiguration extends JPanel {
     JHTMLEditor htmlEditor = new JHTMLEditor(JHTMLEditor.setCustomJavascriptConfiguration(configurationScript));
     htmlEditor.setHTMLContent("<p>The toolbar was modified using custom configuration.</p>");
     add(htmlEditor, BorderLayout.CENTER);
+  }
+  
+  /* Standard main method to try that test as a standalone application. */
+  public static void main(String[] args) {
+    UIUtils.setPreferredLookAndFeel();
+    NativeInterface.open();
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        JFrame frame = new JFrame("DJ Native Swing Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new CustomConfiguration(), BorderLayout.CENTER);
+        frame.setSize(800, 600);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+      }
+    });
   }
   
 }
