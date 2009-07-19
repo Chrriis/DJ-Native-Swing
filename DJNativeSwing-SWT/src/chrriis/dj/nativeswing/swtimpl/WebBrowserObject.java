40fda0a04c7d97942d9cc5642bf70cd4d2662c17
Chrriis <chrriis@gmail.com>
2009-07-19 15:18:54 +0000

    F5 is blocked for plugin based objects to avoid page reload.

--- DJNativeSwing-SWT/src/chrriis/dj/nativeswing/swtimpl/WebBrowserObject.java
+++ DJNativeSwing-SWT/src/chrriis/dj/nativeswing/swtimpl/WebBrowserObject.java
@@ -266,6 +266,15 @@ public abstract class WebBrowserObject {
             "embeddedObject.style.width = '100%';" + LS +
             "embeddedObject.style.height = '100%';" + LS +
             "sendCommand('[Chrriis]WB_setLoaded');" + LS +
+            "window.document.attachEvent(\"onkeydown\", function() {" + LS +
+            "  switch (event.keyCode) {" + LS +
+                 // F5
+            "    case 116 :" + LS +
+            "      event.returnValue = false;" + LS +
+            "      event.keyCode = 0;" + LS +
+            "      break;" + LS +
+            "  }" + LS +
+            "});" + LS +
             "//-->" + LS;
           return getInputStream(content);
         }
