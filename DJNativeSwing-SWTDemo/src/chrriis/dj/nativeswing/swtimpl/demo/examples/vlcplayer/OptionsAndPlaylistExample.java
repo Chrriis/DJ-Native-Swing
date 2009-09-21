8b303ef8d3f720ed3bd2a4ee5458fc9c615eb6ca
Chrriis <chrriis@gmail.com>
2009-09-21 20:48:49 +0000

    No title option demo for VLC video.

--- DJNativeSwing-SWTDemo/src/chrriis/dj/nativeswing/swtimpl/demo/examples/vlcplayer/OptionsAndPlaylistExample.java
+++ DJNativeSwing-SWTDemo/src/chrriis/dj/nativeswing/swtimpl/demo/examples/vlcplayer/OptionsAndPlaylistExample.java
@@ -70,7 +70,7 @@ public class OptionsAndPlaylistExample extends JPanel {
         // Thus, we stop any previously running item, clear the playlist and add our item
         playlist.stop();
         playlist.clear();
-        playlist.addItem(playerFileTextField.getText(), ":start-time=30 :no-audio");
+        playlist.addItem(playerFileTextField.getText(), ":start-time=30 :no-audio :no-video-title-show");
         playlist.play();
       }
     };
