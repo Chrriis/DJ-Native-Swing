cea4758e95f7a1b81d8623b87b35c1a8031452a4
Chrriis <chrriis@gmail.com>
2009-11-07 13:13:16 +0000

    - Local host detection can print debug info. - Threading is improved over reading the local host address.

--- DJNativeSwing/src/chrriis/common/Utils.java
+++ DJNativeSwing/src/chrriis/common/Utils.java
@@ -309,25 +309,28 @@ public class Utils {
    * @return the local host address that was found, or null.
    */
   public static String getLocalHostAddress() {
-    if(localHostAddress != null) {
-      return "".equals(localHostAddress)? null: localHostAddress;
-    }
-    String localHostAddress = System.getProperty("nativeswing.localhostaddress");
-    if("_localhost_".equals(localHostAddress)) {
-      try {
-        localHostAddress = InetAddress.getLocalHost().getHostAddress();
-      } catch(Exception e) {
-        localHostAddress = null;
+    synchronized(Utils.class) {
+      if(localHostAddress != null) {
+        return "".equals(localHostAddress)? null: localHostAddress;
       }
+      String localHostAddress = System.getProperty("nativeswing.localhostaddress");
+      if("_localhost_".equals(localHostAddress)) {
+        try {
+          localHostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch(Exception e) {
+          localHostAddress = null;
+        }
+      }
+      if(localHostAddress == null) {
+        boolean isDebugging = Boolean.parseBoolean(System.getProperty("nativeswing.debug.printlocalhostaddressdetection"));
+        localHostAddress = getLocalHostAddress(0, isDebugging);
+      }
+      if(Boolean.parseBoolean(System.getProperty("nativeswing.debug.printlocalhostaddress"))) {
+        System.err.println("Local host address: " + localHostAddress);
+      }
+      Utils.localHostAddress = localHostAddress == null? "": localHostAddress;
+      return localHostAddress;
     }
-    if(localHostAddress == null) {
-      localHostAddress = getLocalHostAddress(0);
-    }
-    if(Boolean.parseBoolean(System.getProperty("nativeswing.debug.printlocalhostaddress"))) {
-      System.err.println("Local host address: " + localHostAddress);
-    }
-    Utils.localHostAddress = localHostAddress == null? "": localHostAddress;
-    return localHostAddress;
   }
 
   /**
@@ -336,9 +339,25 @@ public class Utils {
    * @return the local host address that was found, or null.
    */
   public static String getLocalHostAddress(int port) {
+    return getLocalHostAddress(port, false);
+  }
+
+  private static String getLocalHostAddress(int port, boolean isDebugging) {
+    if(isDebugging) {
+      System.err.println("Local host address detection using " + (port == 0? "an automatic port": "port " + port) + ":");
+    }
     String loopbackAddress = "127.0.0.1";
+    if(isDebugging) {
+      System.err.print("  Trying 127.0.0.1: ");
+    }
     if(isLocalHostAddressReachable(loopbackAddress, port)) {
+      if(isDebugging) {
+        System.err.println("success.");
+      }
       return loopbackAddress;
+    }
+    if(isDebugging) {
+      System.err.println("failed.");
     }
     List<InetAddress> inetAddressList = new ArrayList<InetAddress>();
     try {
@@ -372,16 +391,39 @@ public class Utils {
         return o1.getHostAddress().compareTo(o2.getHostAddress());
       }
     });
+    if(isDebugging) {
+      System.err.println("  Trying addresses: " + inetAddressList);
+    }
     for(InetAddress address: inetAddressList) {
       String hostAddress = address.getHostAddress();
+      if(isDebugging) {
+        System.err.print("    " + hostAddress + ": ");
+      }
       if(isLocalHostAddressReachable(hostAddress, port)) {
+        if(isDebugging) {
+          System.err.println("success.");
+        }
         return hostAddress;
+      }
+      if(isDebugging) {
+        System.err.println("failed.");
       }
     }
     try {
+      if(isDebugging) {
+        System.err.print("  Trying LocalHost: ");
+      }
       // This call can take some time.
-      return InetAddress.getLocalHost().getHostAddress();
+      String hostAddress = InetAddress.getLocalHost().getHostAddress();
+      if(isDebugging) {
+        System.err.print("success (" + hostAddress + ").");
+      }
+      return hostAddress;
     } catch(Exception e) {
+    }
+    if(isDebugging) {
+      System.err.println("failed.");
+      System.err.println("  Failed to find a suitable local host address!");
     }
     return null;
   }
