fb394ec8e1ea60066d68149d268b8c8847352fd7
Chrriis <chrriis@gmail.com>
2009-11-19 21:02:21 +0000

    NetworkURLClassLoader has a retry mechanism and better error handling.

--- DJNativeSwing-SWT/src/chrriis/common/NetworkURLClassLoader.java
+++ DJNativeSwing-SWT/src/chrriis/common/NetworkURLClassLoader.java
@@ -7,11 +7,12 @@
  */
 package chrriis.common;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * A class loader that loads classes from a given codebase.
@@ -41,27 +42,39 @@ public class NetworkURLClassLoader extends ClassLoader {
 
   @Override
   protected Class<?> findClass(String name) throws ClassNotFoundException {
-    String path = name.replace('.', '/') + ".class";
-    Class<?> clazz = null;
-    InputStream in = new BufferedInputStream(getResourceAsStream(path));
-    try {
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      byte[] bytes = new byte[1024];
-      for(int n; (n=in.read(bytes)) != -1; baos.write(bytes, 0, n)) {
+    Exception exception = null;
+    // Let's retry twice
+    for(int i=0; i<2; i++) {
+      String path = name.replace('.', '/') + ".class";
+      URL resourceURL = getResource(path);
+      InputStream in = null;
+      Class<?> clazz = null;
+      exception = null;
+      try {
+        URLConnection connection = resourceURL.openConnection();
+        connection.setReadTimeout(4000);
+        in = connection.getInputStream();
+//      InputStream in = new BufferedInputStream(getResourceAsStream(path));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] bytes = new byte[1024];
+        for(int n; (n=in.read(bytes)) != -1; baos.write(bytes, 0, n)) {
+        }
+        bytes = baos.toByteArray();
+        clazz = defineClass(name, bytes, 0, bytes.length);
+      } catch(Exception e) {
+        exception = e;
       }
-      bytes = baos.toByteArray();
-      clazz = defineClass(name, bytes, 0, bytes.length);
-    } catch(Exception e) {
-//      e.printStackTrace();
+      try {
+        if(in != null) {
+          in.close();
+        }
+      } catch(Exception e) {
+      }
+      if(clazz != null) {
+        return clazz;
+      }
     }
-    try {
-      in.close();
-    } catch(Exception e) {
-    }
-    if(clazz != null) {
-      return clazz;
-    }
-    return super.findClass(name);
+    throw new ClassNotFoundException(name, exception);
   }
 
   public static void main(String[] args) throws Exception {
@@ -69,8 +82,18 @@ public class NetworkURLClassLoader extends ClassLoader {
     String mainClass = args[1];
     String[] newArgs = new String[args.length - 2];
     System.arraycopy(args, 2, newArgs, 0, newArgs.length);
-    Class<?> clazz = new NetworkURLClassLoader(codeBase).loadClass(mainClass);
-    clazz.getMethod("main", String[].class).invoke(null, new Object[] {newArgs});
+    Class<?> clazz;
+    Method method;
+    try {
+      clazz = new NetworkURLClassLoader(codeBase).loadClass(mainClass);
+      method = clazz.getDeclaredMethod("main", String[].class);
+      method.setAccessible(true);
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.exit(-1);
+      return;
+    }
+    method.invoke(null, new Object[] {newArgs});
   }
 
 }
