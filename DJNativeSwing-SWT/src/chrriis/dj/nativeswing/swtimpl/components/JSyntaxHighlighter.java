7ef01e939e70697ef1bd21488914b82faad580af
Chrriis <chrriis@gmail.com>
2011-03-18 22:27:16 +0000

    Syntax highlighter issue.

--- DJNativeSwing-SWT/src/chrriis/dj/nativeswing/swtimpl/components/JSyntaxHighlighter.java
+++ DJNativeSwing-SWT/src/chrriis/dj/nativeswing/swtimpl/components/JSyntaxHighlighter.java
@@ -132,7 +132,8 @@ public class JSyntaxHighlighter extends NSPanelComponent {
       "  <body id=\"body\">" +
       "    <div class=\"wrapper\">" +
       "      <pre name=\"code\" class=\"" + language.getLanguage() + "\">" +
-      Utils.escapeXML(content) +
+      // Do not call "Utils.escapeXML(content)": "&apos;" must not be replaced.
+      escapeXML(content) +
       "</pre>" + LS +
       "    </div>" +
       "  </body>" + LS +
@@ -143,6 +144,37 @@ public class JSyntaxHighlighter extends NSPanelComponent {
       "</script>" + LS +
       "</html>";
     webBrowser.setHTMLContent(hContent);
+  }
+
+  private static String escapeXML(String s) {
+    if(s == null || s.length() == 0) {
+      return s;
+    }
+    StringBuilder sb = new StringBuilder((int)(s.length() * 1.1));
+    for(int i=0; i<s.length(); i++) {
+      char c = s.charAt(i);
+      switch(c) {
+        case '<':
+          sb.append("&lt;");
+          break;
+        case '>':
+          sb.append("&gt;");
+          break;
+        case '&':
+          sb.append("&amp;");
+          break;
+//        case '\'':
+//          sb.append("&apos;");
+//          break;
+        case '\"':
+          sb.append("&quot;");
+          break;
+        default:
+          sb.append(c);
+        break;
+      }
+    }
+    return sb.toString();
   }
 
 }
