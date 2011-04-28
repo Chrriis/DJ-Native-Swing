1f7c10a6d9194edc58a4fffd59685feea8623618
Chrriis <chrriis@gmail.com>
2011-04-28 20:41:06 +0000

    Updated to also show POST data using embedded web server.

--- DJNativeSwing-SWTDemo/src/chrriis/dj/nativeswing/swtimpl/demo/examples/webbrowser/NavigationParameters.java
+++ DJNativeSwing-SWTDemo/src/chrriis/dj/nativeswing/swtimpl/demo/examples/webbrowser/NavigationParameters.java
@@ -29,6 +29,7 @@ import javax.swing.SwingUtilities;
 import chrriis.common.UIUtils;
 import chrriis.common.Utils;
 import chrriis.common.WebServer;
+import chrriis.common.WebServer.HTTPData;
 import chrriis.common.WebServer.HTTPRequest;
 import chrriis.common.WebServer.WebServerContent;
 import chrriis.dj.nativeswing.swtimpl.NativeInterface;
@@ -44,18 +45,18 @@ public class NavigationParameters extends JPanel {
     super(new BorderLayout());
     JPanel buttonPanel = new JPanel(new GridBagLayout());
     buttonPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-    buttonPanel.add(new JLabel("Custom Header:"), new GridBagConstraints(0, 0, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+    buttonPanel.add(new JLabel("Custom Header:"), new GridBagConstraints(0, 0, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 1, 0), 0, 0));
     final JTextField testHeaderKeyTextField = new JTextField("Custom-header", 10);
-    buttonPanel.add(testHeaderKeyTextField, new GridBagConstraints(1, 0, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 5, 0, 0), 0, 0));
+    buttonPanel.add(testHeaderKeyTextField, new GridBagConstraints(1, 0, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 5, 1, 0), 0, 0));
     final JTextField testHeaderValueTextField = new JTextField("My value", 10);
-    buttonPanel.add(testHeaderValueTextField, new GridBagConstraints(2, 0, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 5, 0, 0), 0, 0));
+    buttonPanel.add(testHeaderValueTextField, new GridBagConstraints(2, 0, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 5, 1, 0), 0, 0));
+    buttonPanel.add(new JLabel("POST Data:"), new GridBagConstraints(0, 1, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(1, 0, 0, 0), 0, 0));
+    final JTextField testPostDataKeyTextField = new JTextField("Custom-POST", 10);
+    buttonPanel.add(testPostDataKeyTextField, new GridBagConstraints(1, 1, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(1, 5, 0, 0), 0, 0));
+    final JTextField testPostDataValueTextField = new JTextField("My value", 10);
+    buttonPanel.add(testPostDataValueTextField, new GridBagConstraints(2, 1, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(1, 5, 0, 0), 0, 0));
     JButton testHeaderButton = new JButton("Test");
-    buttonPanel.add(testHeaderButton, new GridBagConstraints(3, 0, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 5, 0, 0), 0, 0));
-    buttonPanel.add(new JLabel("BugZilla Search:"), new GridBagConstraints(0, 1, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-    final JTextField testPostDataTextField = new JTextField("Browser", 20);
-    buttonPanel.add(testPostDataTextField, new GridBagConstraints(1, 1, 2, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 5, 0, 0), 0, 0));
-    JButton testPostDataButton = new JButton("Go!");
-    buttonPanel.add(testPostDataButton, new GridBagConstraints(3, 1, 1, 1, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 5, 0, 0), 0, 0));
+    buttonPanel.add(testHeaderButton, new GridBagConstraints(3, 0, 1, 2, 0, GridBagConstraints.WEST, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 5, 0, 0), 0, 0));
     add(buttonPanel, BorderLayout.NORTH);
     final JPanel webBrowserPanel = new JPanel(new BorderLayout());
     webBrowserPanel.setBorder(BorderFactory.createTitledBorder("Native Web Browser component"));
@@ -71,27 +72,11 @@ public class NavigationParameters extends JPanel {
         headersMap.put("User-agent", "Native Swing Browser");
         headersMap.put(testHeaderKeyTextField.getText(), testHeaderValueTextField.getText());
         parameters.setHeaders(headersMap);
+        Map<String, String> postDataMap = new HashMap<String, String>();
+        postDataMap.put(testPostDataKeyTextField.getText(), testPostDataValueTextField.getText());
+        parameters.setPostData(postDataMap);
         // Let's generate the page with the resulting HTTP headers dynamically.
         webBrowser.navigate(WebServer.getDefaultWebServer().getDynamicContentURL(NavigationParameters.this.getClass().getName(), "header-viewer.html"), parameters);
-        webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
-        webBrowserPanel.revalidate();
-        webBrowserPanel.repaint();
-      }
-    });
-    testPostDataButton.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        webBrowserPanel.removeAll();
-        JWebBrowser webBrowser = new JWebBrowser();
-        webBrowser.setBarsVisible(false);
-        WebBrowserNavigationParameters parameters = new WebBrowserNavigationParameters();
-        Map<String, String> postDataMap = new HashMap<String, String>();
-        postDataMap.put("short_desc_type", "allwordssubstr");
-        postDataMap.put("short_desc", testPostDataTextField.getText());
-        postDataMap.put("bug_status", "NEW");
-        postDataMap.put("product", "Platform");
-        postDataMap.put("component", "SWT");
-        parameters.setPostData(postDataMap);
-        webBrowser.navigate("https://bugs.eclipse.org/bugs/buglist.cgi", parameters);
         webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
         webBrowserPanel.revalidate();
         webBrowserPanel.repaint();
@@ -106,7 +91,8 @@ public class NavigationParameters extends JPanel {
         @Override
         public InputStream getInputStream() {
           StringBuilder sb = new StringBuilder();
-          sb.append("<html><body><h1>HTTP Headers</h1><table border=\"1\">");
+          sb.append("<html><body>");
+          sb.append("<h1>HTTP Headers</h1><table border=\"1\">");
           Map<String, String> headerMap = httpRequest.getHeaderMap();
           String[] keys = headerMap.keySet().toArray(new String[0]);
           Arrays.sort(keys, String.CASE_INSENSITIVE_ORDER);
@@ -117,7 +103,24 @@ public class NavigationParameters extends JPanel {
             sb.append(Utils.escapeXML(headerMap.get(key)));
             sb.append("</td></tr>");
           }
-          sb.append("</table></body></html>");
+          sb.append("</table>");
+          HTTPData[] httpPostDataArray = httpRequest.getHTTPPostDataArray();
+          if(httpPostDataArray != null) {
+            sb.append("<h1>HTTP POST Data</h1><table border=\"1\">");
+            HTTPData httpPostData = httpPostDataArray[0];
+            Map<String, String> postHeaderMap = httpPostData.getHeaderMap();
+            String[] postKeys = postHeaderMap.keySet().toArray(new String[0]);
+            Arrays.sort(postKeys, String.CASE_INSENSITIVE_ORDER);
+            for(String key: postKeys) {
+              sb.append("<tr><td>");
+              sb.append(Utils.escapeXML(key));
+              sb.append("</td><td>");
+              sb.append(Utils.escapeXML(postHeaderMap.get(key)));
+              sb.append("</td></tr>");
+            }
+            sb.append("</table>");
+          }
+          sb.append("</body></html>");
           return getInputStream(sb.toString());
         }
       };
