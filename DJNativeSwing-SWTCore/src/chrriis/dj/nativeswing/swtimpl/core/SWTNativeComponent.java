5b5574535077cbbfb75c1191db26768ba465cc9b
Christopher Deckers <chrriis@gmail.com>
2022-01-31 11:12:06 +0100

    Code cleanup.

--- DJNativeSwing-SWTCore/src/chrriis/dj/nativeswing/swtimpl/core/SWTNativeComponent.java
+++ DJNativeSwing-SWTCore/src/chrriis/dj/nativeswing/swtimpl/core/SWTNativeComponent.java
@@ -260,8 +260,10 @@ public abstract class SWTNativeComponent extends NativeComponent {
         int height = (Integer)args[1];
         // When there is a scaling factor (e.g. 150%), Swing automatically converts the conceptual sizes (e.g. 400) to physical sizes (e.g. 600).
         // The conceptual sizes that we obtain are not automatically converted in SWT, so we need to manually apply the scaling factor.
-        int currentDpiX = shell.getDisplay().getDPI().x;
-        int currentDpiY = shell.getDisplay().getDPI().y;
+        Display display = shell.getDisplay();
+        Point dpi = display.getDPI();
+        int currentDpiX = dpi.x;
+        int currentDpiY = dpi.y;
         float defaultDpi = 96.0f;
         if(currentDpiX != defaultDpi || currentDpiY != defaultDpi) {
           width = Math.round(width * currentDpiX / defaultDpi);
